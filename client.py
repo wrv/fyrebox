16f15c62cf60cc147b5bebede740f5aad996289b
Natnael Getahun <natnaelg@mit.edu>
2014-11-19 00:00:23 -0500

    Starting to setup the client

---
+++ client.py
@@ -0,0 +1,18 @@
+class File:
+	"""A simple class that represents an in memory file, useful for manipulation on the client side"""
+	def __init__(self, fileName, content, key):
+		self.fileName = fileName
+		self.content = content
+		self.key = key
+	def write(self, content, token):
+		response = sendWrite(self.fileName, content, token)
+		if response != -1:
+			self.content = content
+			return 0
+		return -1
+class WriteError(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
+
