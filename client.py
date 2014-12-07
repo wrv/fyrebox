45cb68043ab85f195415337cec491a0e93d2dab8
Natnael Getahun <natnaelg@mit.edu>
2014-12-07 13:19:17 -0500

    Fixing merge stuff

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 class File:
-	"""A simple class that represents an in memory file, useful for manipulation on the client side"""
-	def __init__(self, fileName, content, key):
+	#"""A simple class that represents an in memory file, useful for manipulation on the client side"""
+	def __init__(self, fileName, content, key, identifier):
 		self.fileName = fileName
 		self.content = content
 		self.key = key
+		self.identifier = identifier
 	def write(self, content, token):
 		response = sendWrite(self.fileName, content, token)
 		if response != -1:
@@ -16,3 +17,11 @@ class WriteError(Exception):
 	def __str__(self):
 		return repr(self.value)
 
+def main():
+	while(1):
+		command = raw_input("$ ")
+		print command
+
+
+if __name__ == "__main__":
+	main()
