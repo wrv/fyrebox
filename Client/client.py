2142786da257636441ea63fbc99dff5729787de9
root <root@vm-6858.localdomain>
2014-12-10 08:54:57 +0000

    Updating client and creating db file

--- Client/client.py
+++ Client/client.py
@@ -7,56 +7,41 @@ import socket
 import json
 from file import *
 from crypto import *
-def helpme(comSplit):
-	print "stuff"
-def create(comSplit):
-	if len(comSplit) != 2:
-		print "Improper command length, create <file name>"
-		return
-	file_key = os.urandom(32)
-	file_name = comSplit[1]
-	content = ""
-	newFile = File(file_name, content, file_key, unique_id)
-def rm(comSplit):
-	print "rm"
-def write(comSplit):
-	print "write"
-def rename(comSplit):
-	print "rename"
-def perm(comSplit):
-	print "perm"
-commands = {		"help"		: helpme,
-			"create" 	: create,
-			"rm" 		: rm,
-			"write"		: write,
-			"rename"	: rename,
-			"perm"		: perm,
-}
+from database import FileInfo, engine
+Session = sessionmaker(bind=engine)
+session = Session()
 openFiles = []
 sslSocket = None
 token = None
 current_directory = None
-class WriteError(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
 
+def helpme(comSplit):
+    print "stuff"
+def create(file_name):
+    file_key = os.urandom(32)
+    open(file_name, 'a').close()
+    file_name = encrypt(file_name, file_key)
+    
+    new_file = FileInfo(file_name = file_name, unique_id = unique_id, file_key = file_key)
+    session.add(new_file)
+    message = {}
+    message['operation'] = "create"
+    message['username'] = username
+    message['token'] = token
+    message['filename'] = file_name
+    message['dirname'] = current_directory
+    
+    
+def rm(comSplit):
+    print "rm"
+def write(comSplit):
+    print "write"
+def rename(comSplit):
+    print "rename"
+def perm(comSplit):
+    print "perm"
 def main():
     serverConnection()
-    login()
-    while(1):
-		command = raw_input("$ ")
-		commandSplit = command.split()
-		if len(commandSplit) == 0:
-			continue
-		if commandSplit[0] in commands:
-			com = commandSplit[0]
-			commands[com](commandSplit)
-			key = os.urandom(32)
-			print decrypt(encrypt("bladlsfjskdfj", key), key)
-		else:
-			print "Unrecognized command " + str(commandSplit[0])
 def serverConnection():
     global sslSocket
     context = SSL.Context(SSL.SSLv23_METHOD)
@@ -64,40 +49,37 @@ def serverConnection():
     s.connect(('localhost', 10023))
     sslSocket = socket.ssl(s)
     response = sslSocket.read()
-    print response
-def login():
-    global sslSocket
-    global token
-    while 1:
-        print "$ Type 1 to register, 2 to login"
-        cmd = raw_input("$ ")
-        message = {}
-        if cmd.strip() == "1":
-            message["operation"] = "register"
-        elif cmd.strip() == "2":
-            message["operation"] = "login"
-        else:
-            print "Improper command"
-            continue
-        username = raw_input("$ username = ")
-        password = raw_input("$ password = ")
-        message["username"] = username
-        message["password"] = password
-        encoded = json.dumps(message)
-        print encoded
-        sslSocket.write(encoded)
-        response = sslSocket.read()
-        response = json.loads(response)
-        print response
-        if 'message' in response:
-                continue
-        token = response['token']
-        if cmd.strip() == "1":
-                os.mkdir(username)
-                os.chdir(username)
-        if cmd.strip() == "2":
-                os.chdir(username)
+    if message in response
         return
-
+    
+def login(username, password):
+    global token
+    global
+    response = authHelper(sslSocket, username, password, "login")
+    if 'message' in response:
+        return
+    token = response['token']
+    current_directory = response['rootdir']
+    os.chdir(current_directory)
+    return
+def register(username, password):
+    global token
+    global current_directory
+    response = authHelper(username, password, "register")
+    if 'message' in response:
+        return
+    token = response['token']
+    current_directory = response['rootdir']
+    os.mkdir(current_directory)
+    os.chdir(current_directory)
+    return
+def authHelper(username, password, command):
+    message = {}
+    message["username"] = username
+    message["password"] = password
+    message["operation"] = command
+    sslSocket.write(json.dumps(message))
+    response = json.loads(sslSocket.read())
+    return response
 if __name__ == "__main__":
-	main()
+    main()
