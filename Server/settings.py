ea156d193cdbdb24157e468c8f5504403dba916d
Nati <natnaelg@mit.edu>
2014-12-12 21:29:11 +0000

    Fixing things and stuff this time

--- Server/settings.py
+++ Server/settings.py
@@ -8,17 +8,17 @@ This file contains all the settings used in fyrebox
 ##############################
 FILE_SERVER_NAME = 'localhost'
 FILE_SERVER_PORT =  10023
-FILE_DEBUG = True
+FILE_DEBUG = False
 
 ##############################
 #  Cloud Server Settings
 ##############################
 CLOUD_SERVER_NAME = 'localhost'
 CLOUD_SERVER_PORT = 10029
-CLOUD_DEBUG = True
+CLOUD_DEBUG = False
 
 ##############################
 #  Client Settings
 ##############################
 CLIENT_SERVER_NAME = 'localhost'
-CLIENT_DEBUG = True
+CLIENT_DEBUG = False
